8616cf8ad0a8c72104e2a2e2e7f11c62e07e4a3c
Dorian Calin <dorin.m.calin@gmail.com>
2023-07-04 17:44:07 +0100

    added api

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,26 @@
 class GamesController < ApplicationController
   def index
     @games = Game.all
+    @search_results = search_game('the witcher 3')
   end
+
 
   def show
     @game = Game.find(params[:id])
+  end
+
+  private
+
+  def search_game(criteria)
+    url = URI("https://opencritic-api.p.rapidapi.com/game/popular")
+    query_params = { criteria: criteria }
+
+    headers = {
+      'X-RapidAPI-Key' => 'ce5d811d70msh33e02a8f92c9be1p1c233ejsn30c592f0d367',
+      'X-RapidAPI-Host' => 'opencritic-api.p.rapidapi.com'
+    }
+
+    response = HTTParty.get(url, query: query_params, headers: headers)
+    # Process the response and handle the data accordingly
   end
 end
